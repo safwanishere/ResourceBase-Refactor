00fe9dad0ca2b55bda5ff9543317007d84f98522
safwan <mohammedsafwan261205@gmail.com>
2025-01-16 23:34:24 +0530

    admin authentication

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, session
+from flask_session import Session
 
 def createApp():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "hadfhdfadfj"
+
+    app.config["SESSION_PERMANENT"] = False
+    app.config["SESSION_TYPE"] = "filesystem"
+    Session(app)
 
     from .views import views
     from .admin import admin
--- website/admin.py
+++ website/admin.py
@@ -1,15 +1,67 @@
-from flask import Blueprint
+import sqlite3
+from flask import Blueprint, session, redirect, render_template, request
+from .helpers import login_required
 
 admin = Blueprint('admin', __name__)
 
-@admin.route('/login')
+@admin.route('/login', methods=["GET", "POST"])
 def login():
-    return "login"
+    
+    # forget any user
+    session.clear()
+
+    if request.method == "POST":
+        if not request.form.get("username"):
+            return redirect("/admin/login")
+
+        elif not request.form.get("password"):
+            return redirect("/admin/login")
+
+        # Query database for username
+        dbcon = sqlite3.connect("database.db")
+        cursor = dbcon.cursor()
+
+        cursor.execute(
+            f"SELECT * FROM admins WHERE username = '{request.form.get('username')}';", 
+        )
+        rows = cursor.fetchall()
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or rows[0][2] != request.form.get('password'):
+            print()
+            print("invalid username or password")
+            print()
+            return redirect("/admin/login")
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0][0]
+
+        cursor.close()
+        dbcon.close()
+
+        # Redirect user to home page
+        return redirect("/admin/upload")
+
+    else:
+        return render_template("login.html")
+
 
 @admin.route('/logout')
 def logout():
-    return "logout"
 
-@admin.route('/upload')
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/admin/login")
+
+
+@admin.route('/upload', methods=["GET", "POST"])
+@login_required
 def upload():
-    return "upload"
+    
+    if request.method == "POST":
+        pass
+
+    else:
+        return render_template("upload.html")
