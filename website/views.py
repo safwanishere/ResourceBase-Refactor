97ad0d7595f1b31bae893d629f30cd762254924c
Akshtih <140843752+Akshtih@users.noreply.github.com>
2025-01-17 21:05:26 +0530

    Added a branch.html page and backend
    for getting coruses on the basis of branch

--- website/views.py
+++ website/views.py
@@ -1,6 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,request
+import sqlite3
 
 views = Blueprint('views', __name__)
+
+def get_db_connection():
+    connection = sqlite3.connect('database.db')
+    connection.row_factory = sqlite3.Row
+    return connection
 
 @views.route('/')
 def index():
@@ -8,7 +14,34 @@ def index():
 
 @views.route('/branch')
 def branch():
-    return "branch"
+    branch_name = request.args.get('branchName')
+
+    if not branch_name:
+        return "Error: 'branchName' parameter is required!", 400
+
+    connection = get_db_connection()
+    cursor = connection.cursor()
+
+    
+    query = """
+        SELECT code, branch, semester, course_name, type
+        FROM courses
+        WHERE branch = ?
+        ORDER BY semester ASC;
+    """
+    cursor.execute(query, (branch_name,))
+    courses = cursor.fetchall()
+    connection.close()
+
+    semesters = {}
+    for course in courses:
+        semester = course['semester']
+        if semester not in semesters:
+            semesters[semester] = []
+        semesters[semester].append(course)
+
+    return render_template('branch.html', branch_name=branch_name, semesters=semesters)
+
 
 @views.route('/course')
 def course():
