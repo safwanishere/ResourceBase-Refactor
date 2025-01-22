389e964c3d3043d424a65dd9850dd5a3fbae1052
safwan <mohammedsafwan261205@gmail.com>
2025-01-22 16:27:37 +0530

    handled resource upload form GET request

--- website/admin.py
+++ website/admin.py
@@ -1,6 +1,19 @@
 import sqlite3
 from flask import Blueprint, session, redirect, render_template, request
-from .helpers import login_required
+from functools import wraps
+
+# helper functions
+def login_required(f):
+
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/admin/login")
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
 
 admin = Blueprint('admin', __name__)
 
@@ -22,7 +35,7 @@ def login():
         cursor = dbcon.cursor()
 
         cursor.execute(
-            f"SELECT * FROM admins WHERE username = '{request.form.get('username')}';", 
+            f"SELECT * FROM admins WHERE username = ?;", (request.form.get('username'),)
         )
         rows = cursor.fetchall()
 
@@ -64,4 +77,38 @@ def upload():
         pass
 
     else:
-        return render_template("upload.html")
+        dbcon = sqlite3.connect("database.db")
+        cursor = dbcon.cursor()
+
+        user_id = session["user_id"]
+        cursor.execute("SELECT * FROM admins WHERE ID = ?;", (user_id,))
+        rows = cursor.fetchall()
+        role = rows[0][3]
+        
+        if role == 'admin':
+            cursor.execute("SELECT DISTINCT(branch) FROM courses;")
+            branches = cursor.fetchall()            
+            cursor.execute("SELECT DISTINCT(course_name), code, semester FROM courses;")
+            courses = cursor.fetchall()
+        else:
+            branches = [role]
+            cursor.execute("SELECT DISTINCT(course_name), code, semester FROM courses WHERE branch = ?;", (role,))
+            courses = cursor.fetchall()
+        
+        categories =  [
+            "Course Syllabus",
+            "Textbooks",
+            "Question Bank",
+            "Lecture Slides",
+            "Lecture Notes",
+            "Definitions and Terminology",
+            "Previous Question Papers",
+            "course syllabus",
+            "Lab Manual",
+            "Lecture Records"
+        ]
+
+        cursor.close()
+        dbcon.close()
+
+        return render_template("upload.html", branches=branches, courses=courses, categories=categories)
