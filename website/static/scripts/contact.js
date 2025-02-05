ddb35c2c6906f2cd3c7807abf451f75067f6ed7f
kranthiyelaboina <kranthiyelaboinahere@gmail.com>
2025-02-05 08:42:56 +0530

    Issue Resolved

--- website/static/scripts/contact.js
+++ website/static/scripts/contact.js
@@ -1,17 +1,11 @@
 (function() {
     'use strict';
     const form = document.getElementById('contactForm');
-    const successModal = new bootstrap.Modal(document.getElementById('successModal'));
 
     form.addEventListener('submit', function(event) {
-        // event.preventDefault();
-        // event.stopPropagation();
-
-        if (form.checkValidity()) {
-            successModal.show();
-            form.reset();
-            form.classList.remove('was-validated');
-        } else {
+        if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
             form.classList.add('was-validated');
         }
     }, false);
